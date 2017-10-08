cfc959a729b6febe8134390e0dd91194a0a9bea7
Phạm Đức Toàn <32049220+1412557@users.noreply.github.com>
2017-10-08 16:41:30 +0700

    Add /albums
    add routes for /album
    add index.html for album

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/articles', to: 'articles#index'
-  get '/posts', to:'posts#index'
+  #get '/articles', to: 'articles#index'
+  resources :articles, only: [:index, :show]
+  #get '/posts', to:'posts#index'
+  resources :photos, only: [:index]
 end
